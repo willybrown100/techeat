0781b6ec14209d187f37f41c6a70918b28bf46a8
Dara Odususi <daraodususi@Daras-MacBook-Pro.local>
2024-06-17 19:51:46 +0100

    Initial commit

---
+++ src/features/aboutus/Testimonials.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Testimonials() {
+    const className = "max-w-[1170px]   w-[90vw] py-4 m-auto";
+  return (
+    <section>
+        <article className={`${className}`}>
+
+<div className="my-6 mb-[4rem]">
+      <div className="w-[14rem] h-[0.2rem] bg-brand m-auto"></div>
+      <h3 className="text-brand font-headings font-semibold text-center">testimonials</h3>
+      <div className="w-[14rem] h-[0.2rem] bg-brand m-auto"></div>
+      <h3 className='text-black'> what our users say</h3>
+      </div>
+        </article>
+
+    </section>
+  )
+}
