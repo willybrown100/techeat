b5d4bfbd11eae7542fb510e6f4ed9c310eaaba4e
DevzekiFaith <zekipossible@gmail.com>
2024-06-20 04:23:04 +0100

    Venddor Page

--- src/Signs-Customer/Signup-Ui/Button.jsx
+++ src/Signs-Customer/Signup-Ui/Button.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+// import { useState } from "react";
 
 const Button = () => {
+  // const [stepper, setStepper] = useState(false);
+  // const ToggleNext = () => {
+  //   setStepper(!stepper);
+  // };
   return (
     <div>
-      <button className="w-full h-[3.2rem] bg-orange-500 mt-[1rem]">
+      <button
+        // onClick={ToggleNext}
+        className="w-full h-[3.2rem] bg-orange-500 mt-[1rem] cursor-pointer"
+      >
         Sign Up
       </button>
     </div>
