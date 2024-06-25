007938a8a3429288067196a73a2a68e78e25fdd5
williams <williamsgoddey11@gmail.com>
2024-06-25 10:36:07 -0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,26 @@ import Login from "./features/signup/SignUp";
 import SignUp from "./features/signup/SignUp";
 import PageNotFound from "./components/PageNotFound";
 import Menu from "./features/menu/Menu";
-import Aboutus from "./features/homePage/Aboutus";
+// import Aboutus from "./features/homePage/Aboutus";
 import AboutUs from "./features/aboutus/AboutUs";
 import TermsOfService from "./features/signin/PrivacyPolicy/TermsOfService";
 import Policy from "./features/signin/PrivacyPolicy/Policy";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
+
+ const queryClient=new QueryClient({
+  defaultOptions:{
+    queries:{
+      staleTime:60 * 1000
+    }
+  }
+ })
 
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
+<ReactQueryDevtools initialIsOpen={false}/>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
@@ -25,7 +38,9 @@ function App() {
         <Route path="signup" element={<SignUp />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
+      <Toaster position="top-center" gutter={12} containerStyle={{margin:"8px"}} toastOptions={{success:{duration:3000}, error:{duration:5000},style:{fontSize:"16px", maxWidth:"500px", padding:"16px 24px",backgroundColour:"white",color:""}}}/>
     </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
