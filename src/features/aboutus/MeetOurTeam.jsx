f01f1e88acb0c2ca88c05cc981f64da15cf65aa7
Dara Odususi <daraodususi@Daras-MacBook-Pro.local>
2024-06-26 14:12:41 +0100

    Initial commit

--- src/features/aboutus/MeetOurTeam.jsx
+++ src/features/aboutus/MeetOurTeam.jsx
@@ -1,85 +1,98 @@
-import React from 'react'
+import React from "react";
 
 export default function MeetOurTeam() {
-    const className = "max-w-[1200px]  w-[90vw] py-[6rem] m-auto";
+  const className = "max-w-[1200px]  w-[90vw] py-[3rem] m-auto";
   return (
-    <section>
-<article className={`${className}`}>
-<div className="my-6 mb-[4rem]">
-      <div className="w-[14rem] h-[0.2rem] bg-brand m-auto"></div>
-      <h3 className="text-brand font-headings font-semibold text-center">meet our team</h3>
-      <div className="w-[14rem] h-[0.2rem] bg-brand m-auto"></div>
-      </div>
+    <section className="bg-black md:bg-transparent">
+      <article className={`${className}`}>
+        <div className="md:my-4 md:mb-[4rem]">
+          <div className="w-[14rem] h-[0.2rem] hidden md:block bg-brand m-auto"></div>
+          <h3 className="text-brand text-[1.7rem] font-headings font-semibold text-center">
+            meet our team
+          </h3>
+          <div className="w-[14rem] h-[0.2rem] hidden md:block bg-brand m-auto"></div>
+        </div>
 
-<div className='grid grid-cols-2 md:grid-cols-4 gap-2'>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-2">
+          <div className="bg-white pb-[5rem] relative  shadow-lg shadow-stone-500/50 rounded-[2rem]">
+            <div>
+              <img src="/images/Rectangle 4269.png" alt="img" />
+            </div>
+            <div className="bg-brand p-4 rounded-[1rem] w-[15rem] left-[50%] absolute text-center translate-x-[-50%] bottom-[3rem]">
+              <h4 className="font-semibold text-xl font-headings mb-0">
+                adebayo micheal
+              </h4>
+              <p className=" mb-0">ceo director</p>
+            </div>
+          </div>
 
-<div className='bg-white pb-[5rem] relative  shadow-lg shadow-stone-500/50 rounded-[2rem]'>
-    <div >
-    <img  src='/images/Rectangle 4269.png' alt='img' />
-    </div>
-    <div className='bg-brand p-4 rounded-[1rem] w-[15rem] left-[50%] absolute text-center translate-x-[-50%] bottom-[3rem]'>
-        <h4 className='font-semibold text-xl font-headings mb-0'>adebayo micheal</h4>
-        <p className=' mb-0'>ceo director</p>
-    </div>
-</div>
+          <div className="bg-white pb-[5rem] relative  shadow-lg shadow-stone-500/50 rounded-[2rem]">
+            <div>
+              <img src="/images/Rectangle 4269 (1).png" alt="img" />
+            </div>
+            <div className="bg-brand p-4 rounded-[1rem] w-[15rem]  left-[50%] absolute text-center translate-x-[-50%] bottom-[3rem]">
+              <h4 className="font-semibold text-xl mb-0 font-headings">
+                amina hassan
+              </h4>
+              <p className=" mb-0">marketing manager</p>
+            </div>
+          </div>
+          <div className="bg-white pb-[5rem] relative  shadow-lg shadow-stone-500/50 rounded-[2rem]">
+            <div>
+              <img src="/images/Rectangle 4269 (2).png" alt="img" />
+            </div>
+            <div className="bg-brand p-4 rounded-[1rem]  left-[50%] md:w-[15rem] w-[9rem]  absolute text-center translate-x-[-50%] bottom-[3rem]">
+              <h4 className="font-semibold font-headings mb-0 text-xl">
+                ijeoma okafor
+              </h4>
+              <p className=" mb-0">customer success</p>
+            </div>
+          </div>
+          <div className="bg-white pb-[5rem] relative  shadow-lg shadow-stone-500/50 rounded-[2rem]">
+            <div>
+              <img src="/images/Rectangle 4269 (3).png" alt="img" />
+            </div>
+            <div className="bg-brand p-4 rounded-[1rem]  left-[50%] md:w-[15rem] w-[9rem]  absolute text-center translate-x-[-50%] bottom-[3rem]">
+              <h4 className="font-semibold text-xl mb-0 font-headings">
+                patience okon
+              </h4>
+              <p className=" mb-0">head of sales</p>
+            </div>
+          </div>
+          <div className="bg-white pb-[5rem] relative  shadow-lg shadow-stone-500/50 rounded-[2rem]">
+            <div>
+              <img src="/images/Rectangle 4269 (4).png" alt="img" />
+            </div>
+            <div className="bg-brand p-2 rounded-[1rem]  left-[50%] md:w-[15rem] w-[9rem]  absolute text-center translate-x-[-50%] bottom-[3rem]">
+              <h4 className="font-semibold font-headings text-xl mb-0">
+                bassey effiong
+              </h4>
+              <p className="font-semibold mb-0">head of logistics</p>
+            </div>
+          </div>
+          <div className="bg-white pb-[5rem] relative  shadow-lg shadow-stone-500/50 rounded-[2rem]">
+            <div>
+              <img src="/images/Rectangle 4269 (5).png" alt="img" />
+            </div>
+            <div className="bg-brand p-2 rounded-[1rem]  left-[50%] absolute  md:w-[15rem] w-[9rem] text-center translate-x-[-50%] bottom-[3rem]">
+              <h4 className="font-semibold font-headings text-xl mb-0">
+                oby udeh
+              </h4>
+              <p className="font-semibold mb-0">human resources</p>
+            </div>
+          </div>
 
-<div className='bg-white pb-[5rem] relative  shadow-lg shadow-stone-500/50 rounded-[2rem]'>
-    <div >
-    <img  src='/images/Rectangle 4269 (1).png' alt='img' />
-    </div>
-    <div className='bg-brand p-4 rounded-[1rem] w-[15rem]  left-[50%] absolute text-center translate-x-[-50%] bottom-[3rem]'>
-        <h4 className='font-semibold text-xl mb-0 font-headings'>amina hassan</h4>
-        <p className=' mb-0'>marketing manager</p>
-    </div>
-</div>
-<div className='bg-white pb-[5rem] relative  shadow-lg shadow-stone-500/50 rounded-[2rem]'>
-    <div >
-    <img  src='/images/Rectangle 4269 (2).png' alt='img' />
-    </div>
-    <div className='bg-brand p-4 rounded-[1rem]  left-[50%] md:w-[15rem] w-[9rem]  absolute text-center translate-x-[-50%] bottom-[3rem]'>
-        <h4 className='font-semibold font-headings mb-0 text-xl'>ijeoma okafor</h4>
-        <p className=' mb-0'>customer success</p>
-    </div>
-</div>
-<div className='bg-white pb-[5rem] relative  shadow-lg shadow-stone-500/50 rounded-[2rem]'>
-    <div >
-    <img  src='/images/Rectangle 4269 (3).png' alt='img' />
-    </div>
-    <div className='bg-brand p-4 rounded-[1rem]  left-[50%] md:w-[15rem] w-[9rem]  absolute text-center translate-x-[-50%] bottom-[3rem]'>
-        <h4 className='font-semibold text-xl mb-0 font-headings'>patience okon</h4>
-        <p className=' mb-0'>head of sales</p>
-    </div>
-</div>
-<div className='bg-white pb-[5rem] relative  shadow-lg shadow-stone-500/50 rounded-[2rem]'>
-    <div >
-    <img  src='/images/Rectangle 4269 (4).png' alt='img' />
-    </div>
-    <div className='bg-brand p-2 rounded-[1rem]  left-[50%] md:w-[15rem] w-[9rem]  absolute text-center translate-x-[-50%] bottom-[3rem]'>
-        <h4 className='font-semibold font-headings text-xl mb-0'>bassey effiong</h4>
-        <p className='font-semibold mb-0'>head of logistics</p>
-    </div>
-</div>
-<div className='bg-white pb-[5rem] relative  shadow-lg shadow-stone-500/50 rounded-[2rem]'>
-    <div >
-    <img  src='/images/Rectangle 4269 (5).png' alt='img' />
-    </div>
-    <div className='bg-brand p-2 rounded-[1rem]  left-[50%] absolute  md:w-[15rem] w-[9rem] text-center translate-x-[-50%] bottom-[3rem]'>
-        <h4 className='font-semibold font-headings text-xl mb-0'>oby udeh</h4>
-        <p className='font-semibold mb-0'>human resources</p>
-    </div>
-</div>
-
-<div className='bg-white pb-[5rem] relative  shadow-lg shadow-stone-500/50 rounded-[2rem]'>
-    <div >
-    <img  src='/images/Rectangle 4269 (6).png' alt='img' />
-    </div>
-    <div className='bg-brand p-4 rounded-[1rem]  left-[50%] absolute md:w-[15rem] w-[9rem] text-center translate-x-[-50%] bottom-[2rem]'>
-        <h4 className='font-semibold font-headings mb-0'>bola oyewole</h4>
-        <p className='font-semibold mb-0'>human resources</p>
-    </div>
-</div>
-</div>
-</article>
+          <div className="bg-white pb-[5rem] relative  shadow-lg shadow-stone-500/50 rounded-[2rem]">
+            <div>
+              <img src="/images/Rectangle 4269 (6).png" alt="img" />
+            </div>
+            <div className="bg-brand p-4 rounded-[1rem]  left-[50%] absolute md:w-[15rem] w-[9rem] text-center translate-x-[-50%] bottom-[2rem]">
+              <h4 className="font-semibold font-headings mb-0">bola oyewole</h4>
+              <p className="font-semibold mb-0">human resources</p>
+            </div>
+          </div>
+        </div>
+      </article>
     </section>
-  )
+  );
 }
